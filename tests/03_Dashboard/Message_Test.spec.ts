34e890750ee0a882564e2775bd7c9248a1072a6c
MdTajwarAliRumman <tajwaralirumman@gmail.com>
2025-12-10 12:26:24 +0600

    Updating_UserProfile_Test

--- tests/03_Dashboard/Message_Test.spec.ts
+++ tests/03_Dashboard/Message_Test.spec.ts
@@ -10,7 +10,7 @@ test.beforeEach(async ({ page }) => {
     await page.locator("//input[contains(@placeholder,'youremail@email.com')]").fill('magif69413@bialode.com')
     await page.locator("//input[@placeholder='******']").fill('12345678Tar!')
     await page.locator("//button[normalize-space()='Log In']").click();
-    await expect(page.locator("//button[normalize-space()='Log In']")).toBeVisible();
+    // await expect(page.locator("//button[normalize-space()='Log In']")).toBeVisible();
 
 })
 
@@ -59,3 +59,9 @@ test('Testing The AI chat if it works properly or not', async ({ page }) => {
     // await expect(page.locator("//h2[normalize-space()='Summary Report:']")).toBeVisible();
     //h2[normalize-space()='Summary Report:']
 });
+
+// test('further messaging', async ({ page }) => {
+
+//     await page.locator("//span[normalize-space()='AI Bot']").click()
+//     await page.locator("//div[contains(@class,'flex min-h-screen bg-//div[4]//button[1]").click()
+// });
--- tests/03_Dashboard/UserProfile_Test.spec.ts
+++ tests/03_Dashboard/UserProfile_Test.spec.ts
@@ -8,7 +8,7 @@ test.beforeEach(async ({ page }) => {
     await page.locator("//input[contains(@placeholder,'youremail@email.com')]").fill('magif69413@bialode.com')
     await page.locator("//input[@placeholder='******']").fill('12345678Tar!')
     await page.locator("//button[normalize-space()='Log In']").click();
-    await expect(page.locator("//button[normalize-space()='Log In']")).toBeVisible();
+    // await expect(page.locator("//button[normalize-space()='Log In']")).toBeVisible();
 
 })
 
@@ -19,11 +19,21 @@ test.afterEach(async ({ page }) => {
 test('Test User profile different sections and fields', async ({ page }) => {
 
     await page.locator("//span[normalize-space()='Profile']").click()
-    await page.locator("//input[@placeholder='Azusa Nakano']").fill('Md. Tajwar Ali')
-    await page.locator("//input[contains(@placeholder,'03393-4933')]").fill('01933945963')
-    await page.locator("//input[@placeholder='R,M,Das Road']").fill('Mohakhali')
-    await page.locator("//input[@placeholder='Azusa Nakano']").fill('Md. Tajwar Ali')
-    await page.locator("//input[@placeholder='Azusa Nakano']").fill('Md. Tajwar Ali')
+    await page.getByRole('textbox', { name: 'Azusa Nakano' }).click();
+    await page.getByRole('textbox', { name: 'Azusa Nakano' }).fill('Tajwar');
+    await page.getByRole('textbox', { name: 'Azusa Nakano' }).press('Tab');
+    await page.getByRole('textbox', { name: 'R,M,Das Road' }).click();
+    await page.getByRole('textbox', { name: 'R,M,Das Road' }).fill('501 a road');
+    await page.getByRole('textbox', { name: 'R,M,Das Road' }).press('Tab');
+    await page.getByRole('textbox', { name: '-4933' }).click();
+    await page.getByRole('textbox', { name: '-4933' }).fill('0244405504');
+    await page.locator('select[name="country"]').selectOption('Bangladesh');
+    await page.locator('input[name="city"]').click();
+    await page.locator('input[name="city"]').fill('Dhaka');
+    await page.locator('input[name="zip_code"]').click();
+    await page.locator('input[name="zip_code"]').fill('1234');
+    await page.locator('select[name="language"]').selectOption('Bengali');
+    await page.getByRole('button', { name: 'Save' }).first().click();
 
 
 });
